2ebf83f17807fce865d6f512f2076092a40113af
Denis Yagofarov <denyago@gmail.com>
2012-05-25 20:52:26 +0300

    Small refactoring of session keys storage

--- lib/devise_cas_authenticatable/single_sign_out/strategies/rails_cache.rb
+++ lib/devise_cas_authenticatable/single_sign_out/strategies/rails_cache.rb
@@ -4,13 +4,13 @@ module DeviseCasAuthenticatable
       class RailsCache < Base
           def store_session_id_for_index(session_index, session_id)
             logger.debug("Storing #{session_id} for index #{session_index}")
-            sids = Rails.cache.read(cache_key(session_index)) || []
+            sids = find_session_id_by_index(session_index)
             sids << session_id
             Rails.cache.write(cache_key(session_index), sids)
           end
 
           def find_session_id_by_index(session_index)
-            sids = Rails.cache.read(cache_key(session_index))
+            sids = Rails.cache.read(cache_key(session_index)) || []
             logger.debug("Found session id(s) #{sids.inspect} for index #{session_index}")
             sids
           end
@@ -22,7 +22,7 @@ module DeviseCasAuthenticatable
 
           def delete_session_id_by_index(session_index, session_id)
             logger.debug("Deleting #{session_id} for index #{session_index}")
-            sids = Rails.cache.read(cache_key(session_index)) || []
+            sids = find_session_id_by_index(session_index)
             logger.debug("Failed to delete #{session_id} for index #{session_index}") unless sids.delete(session_id)
             Rails.cache.write(cache_key(session_index), sids)
           end
